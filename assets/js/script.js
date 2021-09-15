1d466e3684903c6a8b170de5109ced1b944e83af
yinzhedy <yinzhe.dy@gmail.com>
2021-09-14 22:21:39 -0400

    added in incorrect and correct tracker display in html, added corresponding vars into js, started pseudocoding js.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 var secondsLeftNumber = document.querySelector(".seconds-left-number");
 var highScoresText = document.querySelector(".high-scores-text");
+var numberCorrectDisplay = document.querySelector(".number-correct-display");
+var numberIncorrectDisplay = document.querySelector(".number-incorrect-display");
 var quizTitle = document.querySelector(".quiz-title");
 var instructionText = document.querySelector(".instruction-text");
 var startButton = document.querySelector(".start-button");
@@ -7,3 +9,11 @@ var button1 = document.querySelector(".button-1");
 var button2 = document.querySelector(".button-2");
 var button3 = document.querySelector(".button-3");
 var button4 = document.querySelector(".button-4");
+
+// function to begin quiz
+// start timer
+// change -data-visibility on answer buttons, start button, and title
+// change the text inside of instruction text
+// change the text inside of answer buttons
+// either add or subtract time from the timer
+// call the next function for the next question/answers
