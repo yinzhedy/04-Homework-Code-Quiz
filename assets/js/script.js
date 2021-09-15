4244c01e552a49320d0e90407b02f65a81a26f7a
yinzhedy <yinzhe.dy@gmail.com>
2021-09-15 00:41:25 -0400

    fixed display question set functions so that they trigger the next set to appear.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ var numberCorrectDisplay = document.querySelector(".number-correct-display");
 var numberIncorrectDisplay = document.querySelector(".number-incorrect-display");
 var quizTitle = document.querySelector(".quiz-title");
 var instructionText = document.querySelector(".instruction-text");
+var button = document.querySelectorAll("button");
 var startButton = document.querySelector(".start-button");
 var button1 = document.querySelector(".button-1");
 var button2 = document.querySelector(".button-2");
@@ -26,6 +27,7 @@ startButton.addEventListener("click", function() {
     displayQuestionSet1();
     changeStartVisibility();
     changeButtonVisibility();
+    return;
 });
 
 
@@ -66,7 +68,16 @@ function displayQuestionSet1 () {
     button2.textContent = "Red";
     button3.textContent = "Green";
     button4.textContent = "Orange";
-};
+    button.forEach(item => {
+        item.addEventListener('click', event => {
+            if (item == button1) {
+                secondsLeftNumber += 10;}
+            else {
+                secondsleftNumber -= 5;};
+    displayQuestionSet2();
+    });
+});
+}
 
 function displayQuestionSet2 () {
     instructionText.textContent = "Whats your favorite food??"
@@ -74,7 +85,16 @@ function displayQuestionSet2 () {
     button2.textContent = "Curry";
     button3.textContent = "Sushi";
     button4.textContent = "Salad";
-};
+    button.forEach(item => {
+        item.addEventListener('click', event => {
+            if (item == button1) {
+                secondsLeftNumber += 10;}
+            else {
+                secondsleftNumber -= 5;};
+    displayQuestionSet3();
+    });
+});
+}
 
 function displayQuestionSet3 () {
     instructionText.textContent = "Whats your favorite dog breed?"
@@ -82,7 +102,16 @@ function displayQuestionSet3 () {
     button2.textContent = "Chihuahua";
     button3.textContent = "Golden Retriever";
     button4.textContent = "Corgi";
-};
+    button.forEach(item => {
+        item.addEventListener('click', event => {
+            if (item == button1) {
+                secondsLeftNumber += 10;}
+            else {
+                secondsleftNumber -= 5;};
+    displayQuestionSet4();
+    });
+});
+}
 
 function displayQuestionSet4 () {
     instructionText.textContent = "Whats your favorite music?"
@@ -90,7 +119,16 @@ function displayQuestionSet4 () {
     button2.textContent = "Rap";
     button3.textContent = "Pop";
     button4.textContent = "Metal";
-};
+    button.forEach(item => {
+        item.addEventListener('click', event => {
+            if (item == button1) {
+                secondsLeftNumber += 10;}
+            else {
+                secondsleftNumber -= 5;};
+    displayQuestionSet5();
+    });
+});
+}
 
 function displayQuestionSet5 () {
     instructionText.textContent = "Whats your favorite season?"
@@ -98,11 +136,13 @@ function displayQuestionSet5 () {
     button2.textContent = "Summer";
     button3.textContent = "Fall";
     button4.textContent = "Winter";
-};
-
-
-
-// change the text inside of answer buttons
-// either add or subtract time from the timer
-// add to incorrect or correct score display
-// call the next function for the next question/answers
+    button.forEach(item => {
+        item.addEventListener('click', event => {
+            if (item == button1) {
+                secondsLeftNumber += 10;}
+            else {
+                secondsleftNumber -= 5;};
+    displayQuestionSet1();
+    });
+});
+}
