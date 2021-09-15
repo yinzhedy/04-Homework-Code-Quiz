b95259b1cdce6027fc0d574f223f198debb8162e
yinzhedy <yinzhe.dy@gmail.com>
2021-09-14 23:31:18 -0400

    added functions for timer, hiding/shower answer and start buttons, and functions for displaying question and answer sets later on.

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,11 +9,100 @@ var button1 = document.querySelector(".button-1");
 var button2 = document.querySelector(".button-2");
 var button3 = document.querySelector(".button-3");
 var button4 = document.querySelector(".button-4");
+var buttonArray = [button1, button2, button3, button4]
 
+
+startButton.addEventListener("click", function() {
+    var timeLeft = 30;
+
+    timeInterval = setInterval(function () {
+        timeLeft--;
+        secondsLeftNumber.textContent = timeLeft;
+        console.log(timeLeft);
+
+        if(timeLeft === 0) {
+        clearInterval(timeInterval);}
+    }, 1000);
+    displayQuestionSet1();
+    changeStartVisibility();
+    changeButtonVisibility();
+});
 // function to begin quiz
 // start timer
+
+
 // change -data-visibility on answer buttons, start button, and title
 // change the text inside of instruction text
+function changeStartVisibility () {
+    var visibility = startButton.getAttribute("data-visibility");
+
+    if(visibility === "visible") {
+        startButton.setAttribute("display", "none");
+        startButton.setAttribute("data-visibility", "hidden");}
+    else {
+        startButton.removeAttribute("display", "none");
+        startButton.setAttribute("data-visibility", "visible");}
+}
+
+function changeButtonVisibility() {
+    for (let i = 0; i < buttonArray.length; i++) {
+        
+    // startButton.setAttribute("data-visibility", "hidden");
+    // buttonArray.setAttribute("data-visibility", "visible");
+    var visibility = buttonArray[i].getAttribute("data-visibility");
+
+        if(visibility === "hidden") {
+         buttonArray[i].removeAttribute("display", "none");
+        buttonArray[i].setAttribute("data-visibility", "visible");}
+        else {
+        buttonArray[i].setAttribute("display", "none");
+        buttonArray[i].setAttribute("data-visibility", "hidden");} 
+    }
+;}
+
+function displayQuestionSet1 () {
+    instructionText.textContent = "Whats your favorite color?"
+    button1.textContent = "Blue";
+    button2.textContent = "Red";
+    button3.textContent = "Green";
+    button4.textContent = "Orange";
+};
+
+function displayQuestionSet2 () {
+    instructionText.textContent = "Whats your favorite food??"
+    button1.textContent = "Burgers";
+    button2.textContent = "Curry";
+    button3.textContent = "Sushi";
+    button4.textContent = "Salad";
+};
+
+function displayQuestionSet3 () {
+    instructionText.textContent = "Whats your favorite dog breed?"
+    button1.textContent = "Doberman";
+    button2.textContent = "Chihuahua";
+    button3.textContent = "Golden Retriever";
+    button4.textContent = "Corgi";
+};
+
+function displayQuestionSet4 () {
+    instructionText.textContent = "Whats your favorite music?"
+    button1.textContent = "RnB";
+    button2.textContent = "Rap";
+    button3.textContent = "Pop";
+    button4.textContent = "Metal";
+};
+
+function displayQuestionSet5 () {
+    instructionText.textContent = "Whats your favorite season?"
+    button1.textContent = "Spring";
+    button2.textContent = "Summer";
+    button3.textContent = "Fall";
+    button4.textContent = "Winter";
+};
+
+
+
 // change the text inside of answer buttons
 // either add or subtract time from the timer
+// add to incorrect or correct score display
 // call the next function for the next question/answers
